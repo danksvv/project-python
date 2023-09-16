daa01b9ae731a440a6a0f2c5f4bb1b7d6e4a94f3
danks <74599036+danksvv@users.noreply.github.com>
2023-09-16 17:06:39 -0400

    this is a new file of verify of age

---
+++ basic-exercises/age_verify.py
@@ -0,0 +1,10 @@
+# this is test for test you age 
+
+
+age = int(input("Please enter you age : "))
+
+
+if age < 18:
+    print("You age is " + str(age) + ", you are teeneger...")
+else:
+    print("You age is " + str(age) + ", you are young...")
